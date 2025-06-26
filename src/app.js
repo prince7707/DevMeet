509b6e8b084befc9f86bc67e8a100c4d0a3e192f
Prince <70007prince@gmail.com>
2025-06-26 23:55:28 +0530

    Multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -7,26 +7,34 @@ const app = express();
 // });
 
 // This will only handle GET cal to /user
-app.get("/user", (req,res) =>{
-    res.send({firstName: "Prince", College: "MSIT"});
+// app.get("/user", (req,res) =>{
+//     res.send({firstName: "Prince", College: "MSIT"});
 
-})
+// })
 
 app.post("/user", (req,res) =>{
-    //Save Data to the database
+   // Save Data to the database
     res.send("Data saved successfully to the database");
 });
 
-app.delete("/user", (req,res) =>{
-    //Delete Data from the database
-    res.send("Data deleted successfully from the database");
-});
+// app.delete("/user", (req,res) =>{
+//     //Delete Data from the database
+//     res.send("Data deleted successfully from the database");
+// });
 
 
 // This will match all the HTTP method API calls to /test
-app.use("/test",(req,res) => {
-    res.send("hello from the server");
-});
+app.use("/test",(req,res,next) => {
+    // res.send("hello from the server");
+    console.log("Handling the route test");
+    res.send("1st Response");
+    next();
+},
+(req,res)=> {
+    console.log("Handling the second route 2!!");
+    res.send("2nd Response");
+}
+);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");  
